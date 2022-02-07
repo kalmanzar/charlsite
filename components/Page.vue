84a1a518471500cf53ccaec2c9c936c65b91ec43
Kevin Almanzar <kevinalmanzar8@gmail.com>
2022-02-06 20:06:01 -0500

    noSelect transparent highlight color, thin unobstrusive outline over focused items, added netlify deploy support

--- components/Page.vue
+++ components/Page.vue
@@ -1,10 +1,10 @@
 <template>
   <ul class="flex flex-1 flex-col imageLinkList">
-    <nuxt-link to="/story" class="flex imageLink"><img src="/storyOlive.png" class="imageLinkItem" /></nuxt-link>
-    <nuxt-link to="/comics" class="flex imageLink"><img src="/comicsErythro.png" class="imageLinkItem" /></nuxt-link>
-    <nuxt-link to="/digital" class="flex imageLink"><img src="/digitalIllo.png" class="imageLinkItem" /></nuxt-link>
-    <nuxt-link to="/traditional" class="flex imageLink"><img src="/traditionalRunning.png" class="imageLinkItem" /></nuxt-link>
-    <nuxt-link to="/about" class="flex imageLink"><img src="/aboutSelf.png" class="imageLinkItem" /></nuxt-link>
+    <nuxt-link to="/story" class="flex imageLink noSelect"><img src="/storyOlive.png" class="imageLinkItem" /></nuxt-link>
+    <nuxt-link to="/comics" class="flex imageLink noSelect"><img src="/comicsErythro.png" class="imageLinkItem" /></nuxt-link>
+    <nuxt-link to="/digital" class="flex imageLink noSelect"><img src="/digitalIllo.png" class="imageLinkItem" /></nuxt-link>
+    <nuxt-link to="/traditional" class="flex imageLink noSelect"><img src="/traditionalRunning.png" class="imageLinkItem" /></nuxt-link>
+    <nuxt-link to="/about" class="flex imageLink noSelect"><img src="/aboutSelf.png" class="imageLinkItem" /></nuxt-link>
   </ul>
 </template>
 
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,13 +2,13 @@
   <div class="flex siteBackground">
     <div class="flex flex-col wrapper">
       <div class="flex flex-1 flex-col card">
-        <nuxt-link to="/" class="flex imageLink"><img src="/titleCard.png" class="titleCardItem" /></nuxt-link>
+        <nuxt-link to="/" class="flex imageLink noSelect"><img src="/titleCard.png" class="titleCardItem" /></nuxt-link>
         <ul class="flex flex-row flex-wrap tabList">
-          <li class="flex-auto text-center"><nuxt-link to="/story">story</nuxt-link></li>
-          <li class="flex-auto text-center"><nuxt-link to="/comics">comics</nuxt-link></li>
-          <li class="flex-auto text-center"><nuxt-link to="/digital">digital</nuxt-link></li>
-          <li class="flex-auto text-center"><nuxt-link to="/traditional">traditional</nuxt-link></li>
-          <li class="flex-auto text-center"><nuxt-link to="/about">about</nuxt-link></li>
+          <li class="flex-auto text-center"><nuxt-link to="/story" class="noSelect">story</nuxt-link></li>
+          <li class="flex-auto text-center"><nuxt-link to="/comics" class="noSelect">comics</nuxt-link></li>
+          <li class="flex-auto text-center"><nuxt-link to="/digital" class="noSelect">digital</nuxt-link></li>
+          <li class="flex-auto text-center"><nuxt-link to="/traditional" class="noSelect">traditional</nuxt-link></li>
+          <li class="flex-auto text-center"><nuxt-link to="/about" class="noSelect">about</nuxt-link></li>
         </ul>
         <div class="flex flex-1">
           <Nuxt />
